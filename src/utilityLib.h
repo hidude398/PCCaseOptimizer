5bd23715b325e384e17a31c301588817bb3a50b3
Wyatt Whitfield <wyatt.r.whitfield@gmail.com>
2022-08-13 02:52:40 -0400

    Created utilityLib header and populated it with the airUtility class.
    This header provides the definitions for various utilities upon which the
    simulations are founded.
    
    Signed-off-by: hidude398

---
+++ src/utilityLib.h
@@ -0,0 +1,44 @@
+#ifndef utilityLib
+#define utilityLib
+#endif
+
+namespace utilities 
+{
+class airFunctions 
+{
+	template <typename T>
+	T massFlow (T pressure, T density);
+	/* Calculates the flow of mass given the volume flowing plus
+	 * the density of the gas. */
+
+	float estSaturationVaporPressure (float temperature);
+	/* Utilizes the Herman Wobus method to calculate satruation
+	 * vapor pressure of water at a given temperature. This is accurate
+	 * between -50 < T < 100, given a T in celsius. Return value is
+	 * given in mbar */
+
+	template <typename T>
+	T waterVaporPressure (T saturationVaporPressure, int relativeHumidity);
+	/* Returns the actual water vapor pressure given a saturation vapor
+	 * pressure and the measured relative humididty for the air
+	 * conditions */
+
+	template <typename T>
+	T dryAirPressure (T totalPressure, T waterVaporPressure);
+	/* Returns the partial pressure of dry air given a total pressure
+	 * and the known water vapor pressure in the gas */
+
+	template <typename T>
+	T rho (T dryAirPressure, T waterVaporPressure, T temperature);
+	/* Given a particular set of pressures and a temperature in kelvin,
+	 * returns the density of air in kg/m^3 */
+
+	template <typename T>
+	T flowVelocity (T volumeFlow, T area);
+	/* Given a volume flow and the outlet area, calculate the flow
+	 * velocity. For numerical accuracy the volume and area units must
+	 * derive from the same unit of measure. For example, cubic meters
+	 * square meters will be numerically correct, while cubic meters and
+	 * square feet will need a correction factor applied to be accurate */
+}
+} 
